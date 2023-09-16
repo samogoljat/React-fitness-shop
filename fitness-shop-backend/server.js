c66e39d9444725542461b81fe526c3cdaf34f5d9
samogoljat <samo.goljat1@gmail.com>
2023-09-16 20:31:43 +0200

    succesful connect to mongodb

--- fitness-shop-backend/server.js
+++ fitness-shop-backend/server.js
@@ -1,19 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 5000;
 
-mongoose.connect("mongodb://localhost/fitnessShopDB", {
+// MongoDB connection
+const DATABASE_NAME = 'test'; // Replace 'mydatabase' with your desired database name
+mongoose.connect(`mongodb://0.0.0.0:27017/${DATABASE_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
+
+db.once('open', () => {
+  console.log("Successfully connected to MongoDB!");
+});
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+
+//route
+app.get('/home', (req, res) => {
+  res.send('<h1>Welcome to Fitness HomeSite</h1>')  
+})
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
+
