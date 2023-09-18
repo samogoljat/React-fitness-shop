8e65cc843163f09d8caede5f2a8bab47c00c5eed
samogoljat <samo.goljat1@gmail.com>
2023-09-18 19:17:09 +0200

    navbar styling

--- fitness-shop-frontend/src/components/Navbar.js
+++ fitness-shop-frontend/src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "../styles/Navbar.css";  // Adjust the path according to your project structure
 
 const Navbar = () => {
   return (
-    <header className="App-header">
-      <h1>Gym Products</h1>
-      <nav>
-        <button>Home</button>
-        <button>Shop</button>
-        <button>About</button>
-        <button>Contact</button>
-      </nav>
-    </header>
+    <div className="navbar">
+      <div className="navbar-content">
+        <a href="/" className="navbar-brand">Gym Products</a>
+        <ul className="navbar-tabs">
+          <li className="navbar-tab"><a href="/">Home</a></li>
+          <li className="navbar-tab"><a href="/shop">Shop</a></li>
+          <li className="navbar-tab"><a href="/about">About</a></li>
+          <li className="navbar-tab"><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
