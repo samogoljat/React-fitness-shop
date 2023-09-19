2fc12693a58879de8b5ebee958e9cf55d016b99d
samogoljat <samo.goljat1@gmail.com>
2023-09-19 05:32:58 +0200

    Fixed title of website

--- fitness-shop-frontend/src/App.js
+++ fitness-shop-frontend/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Gym Shop";
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
