5d8ff882f57aa86282186261a741149d585c2db3
samogoljat <samo.goljat1@gmail.com>
2023-09-28 18:55:19 +0200

    fixed the items in mongodb

--- fitness-shop-frontend/src/helpers/fetchProducts.js
+++ fitness-shop-frontend/src/helpers/fetchProducts.js
@@ -2,19 +2,19 @@
 import axios from 'axios';
 
 export const fetchProducts = async () => {
-    try {
-        const response = await axios.get('http://localhost:5000/products');
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        return [];
-    }
+  try {
+    const response = await axios.get('http://localhost:5000/products');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return [];
+  }
 };
 
 export const nameChange = (name) => {
   const formattedName = name.toLowerCase().split(' ').join('_');
-  if (name === "Medicine Ball 5kg" || name === "Barbell Set") {
+  if (name === 'Medicine Ball 5kg' || name === 'Barbell Set') {
     return formattedName + '.jpg';
   }
   return formattedName + '.webp';
-}
+};
