964fb6213ec5fcb01e0da378887cf5811e633808
samogoljat <samo.goljat1@gmail.com>
2023-10-06 17:34:36 +0200

    change

--- fitness-shop-frontend/src/components/Home.js
+++ fitness-shop-frontend/src/components/Home.js
@@ -1,16 +1,18 @@
 // MainContent.js
-import React from "react";
-import "../styles/Home.css";
+import React from 'react';
+import '../styles/Home.css';
 
 function MainContent() {
   return (
     <div className="Home-container">
       <h1>Welcome to Gym Bro's</h1>
-      <p>Join us and begin your journey towards a healthier lifestyle.</p>
+      <p>
+        Join us and begin your journey towards a healthier lifestyle. Oh yeah.
+      </p>
       <button
         className="explore-btn"
         onClick={() => {
-          console.log("Explore button clicked!");
+          console.log('Explore button clicked!');
         }}
       >
         Explore
