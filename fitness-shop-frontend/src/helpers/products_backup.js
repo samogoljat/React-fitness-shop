f9283e057a26716438cea0fc7147d447be9017de
samogoljat <samo.goljat1@gmail.com>
2023-10-07 21:26:19 +0200

    only_frontend

--- fitness-shop-frontend/src/helpers/products_backup.js
+++ fitness-shop-frontend/src/helpers/products_backup.js
@@ -1,54 +1,49 @@
 // products.js
 
-import dumbell from '../assets/gym/dumbell.webp';
+import dumbbell_10kg from '../assets/gym/dumbbell_10kg.webp';
 import treadmill from '../assets/gym/treadmill.webp';
-import yogamat from '../assets/gym/yogamat.webp';
-import bands from '../assets/gym/bands.webp';
-import kettlebell from '../assets/gym/kettlebell.webp';
-import rowingmachine from '../assets/gym/rowingmachine.webp';
-import foamroller from '../assets/gym/foamroller.webp';
-import jumprope from '../assets/gym/jumprope.webp';
-import barbellset from '../assets/gym/barbellset.jpg';
-import bench from '../assets/gym/bench.webp';
-import ellipticalmachine from '../assets/gym/ellipticalmachine.webp';
-import fitnesstracker from '../assets/gym/fitnesstracker.webp';
-import gymbag from '../assets/gym/gymbag.webp';
-import medicineball from '../assets/gym/medicineball.jpg';
-import pilatesring from '../assets/gym/pilatesring.webp';
-import pullup from '../assets/gym/pullup.webp';
-import pushup from '../assets/gym/pushup.webp';
-import stabilityball from '../assets/gym/stabilityball.webp';
-import waterbottle from '../assets/gym/waterbottle.webp';
-import yogablock from '../assets/gym/yogablock.webp';
-import wheyprotein from '../assets/gym/wheyprotein.webp';
+import yoga_mat from '../assets/gym/yoga_mat.webp';
+import resistance_bands from '../assets/gym/resistance_bands.webp';
+import kettlebell_15kg from '../assets/gym/kettlebell_15kg.webp';
+import rowing_machine from '../assets/gym/rowing_machine.webp';
+import foam_roller from '../assets/gym/foam_roller.webp';
+import jump_rope from '../assets/gym/jump_rope.webp';
+import barbell_set from '../assets/gym/barbell_set.jpg'; // Note the .jpg extension
+import weight_bench from '../assets/gym/weight_bench.webp';
+import elliptical_machine from '../assets/gym/elliptical_machine.webp';
+import medicine_ball_5kg from '../assets/gym/medicine_ball_5kg.jpg'; // Note the .jpg extension
+import pull_up_bar from '../assets/gym/pull_up_bar.webp';
+import stability_ball from '../assets/gym/stability_ball.webp';
+import pilates_ring from '../assets/gym/pilates_ring.webp';
+import yoga_block from '../assets/gym/yoga_block.webp';
+import push_up_grips from '../assets/gym/push_up_grips.webp';
+import fitness_tracker from '../assets/gym/fitness_tracker.webp';
+import water_bottle from '../assets/gym/water_bottle.webp';
+import gym_bag from '../assets/gym/gym_bag.webp';
+import whey_protein from '../assets/gym/whey_protein.webp';
 
 const products = [
-  { id: 1, name: 'Dumbbell (10kg)', price: '$50', image: dumbell },
+  { id: 1, name: 'Dumbbell 10kg', price: '$50', image: dumbbell_10kg },
   { id: 2, name: 'Treadmill', price: '$500', image: treadmill },
-  { id: 3, name: 'Yoga Mat', price: '$25', image: yogamat },
-  { id: 4, name: 'Resistance Bands', price: '$15', image: bands },
-  { id: 5, name: 'Kettlebell (15kg)', price: '$45', image: kettlebell },
-  { id: 6, name: 'Rowing Machine', price: '$300', image: rowingmachine },
-  { id: 7, name: 'Foam Roller', price: '$20', image: foamroller },
-  { id: 8, name: 'Jump Rope', price: '$10', image: jumprope },
-  { id: 9, name: 'Barbell Set', price: '$120', image: barbellset },
-  { id: 10, name: 'Weight Bench', price: '$150', image: bench },
-  {
-    id: 11,
-    name: 'Elliptical Machine',
-    price: '$350',
-    image: ellipticalmachine,
-  },
-  { id: 12, name: 'Medicine Ball (5kg)', price: '$30', image: medicineball },
-  { id: 13, name: 'Pull-Up Bar', price: '$40', image: pullup },
-  { id: 14, name: 'Stability Ball', price: '$35', image: stabilityball },
-  { id: 15, name: 'Pilates Ring', price: '$20', image: pilatesring },
-  { id: 16, name: 'Yoga Block', price: '$10', image: yogablock },
-  { id: 17, name: 'Push-Up Grips', price: '$15', image: pushup },
-  { id: 18, name: 'Fitness Tracker', price: '$100', image: fitnesstracker },
-  { id: 19, name: 'Water Bottle', price: '$12', image: waterbottle },
-  { id: 20, name: 'Gym Bag', price: '$45', image: gymbag },
-  { id: 20, name: 'Whey Protein', price: '$20', image: wheyprotein },
+  { id: 3, name: 'Yoga Mat', price: '$25', image: yoga_mat },
+  { id: 4, name: 'Resistance Bands', price: '$15', image: resistance_bands },
+  { id: 5, name: 'Kettlebell 15kg', price: '$45', image: kettlebell_15kg },
+  { id: 6, name: 'Rowing Machine', price: '$300', image: rowing_machine },
+  { id: 7, name: 'Foam Roller', price: '$20', image: foam_roller },
+  { id: 8, name: 'Jump Rope', price: '$10', image: jump_rope },
+  { id: 9, name: 'Barbell Set', price: '$120', image: barbell_set },
+  { id: 10, name: 'Weight Bench', price: '$150', image: weight_bench },
+  { id: 11, name: 'Elliptical Machine', price: '$350', image: elliptical_machine },
+  { id: 12, name: 'Medicine Ball 5kg', price: '$30', image: medicine_ball_5kg },
+  { id: 13, name: 'Pull-Up Bar', price: '$40', image: pull_up_bar },
+  { id: 14, name: 'Stability Ball', price: '$35', image: stability_ball },
+  { id: 15, name: 'Pilates Ring', price: '$20', image: pilates_ring },
+  { id: 16, name: 'Yoga Block', price: '$10', image: yoga_block },
+  { id: 17, name: 'Push-Up Grips', price: '$15', image: push_up_grips },
+  { id: 18, name: 'Fitness Tracker', price: '$100', image: fitness_tracker },
+  { id: 19, name: 'Water Bottle', price: '$12', image: water_bottle },
+  { id: 20, name: 'Gym Bag', price: '$45', image: gym_bag },
+  { id: 21, name: 'Whey Protein', price: '$20', image: whey_protein },
 ];
 
 export default products;
