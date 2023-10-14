581b8a46c3e332257648403ed99b620591e6fe89
samogoljat <samo.goljat1@gmail.com>
2023-10-14 07:03:57 +0200

    improved cart functionality

--- fitness-shop-frontend/src/components/Cart.js
+++ fitness-shop-frontend/src/components/Cart.js
@@ -1,35 +1,54 @@
-// fitness-shop-frontend/src/components
 import React from 'react';
 import { useCart } from '../components/CartContext';
 import '../styles/Cart.css';
 
 const Cart = () => {
-    const { cart } = useCart();
+  const { cart } = useCart();
 
-    return (
-        <div className="cart">
-            <h2>Your Cart</h2>
-            {cart.length === 0 ? (
-                <p>Your cart is empty</p>
-            ) : (
-                <ul>
-                    {cart.map((product, index) => (
-                        <li key={index}>
-                            <img src={product.image} alt={product.name} />
-                            <h3>{product.name}</h3>
-                            <p>{product.price}</p>
-                            {/* Additional product details and functionality (like remove from cart) can be added here */}
-                        </li>
-                    ))}
-                </ul>
-            )}
-            {cart.length > 0 && (
-                <p>
-                    Total: ${cart.reduce((total, product) => total + product.price, 0)}
-                </p>
-            )}
-        </div>
-    );
+  const formatPrice = (price) => {
+      if (typeof price !== "number") {
+          console.warn("Unexpected price type:", price);
+          return "$0.00";
+      }
+      return `$${price.toFixed(2)}`;
+  }
+
+  return (
+      <div className="cart">
+          <h2>Your Cart</h2>
+          {cart.length === 0 ? (
+              <p>Your cart is empty</p>
+          ) : (
+              <ul>
+                  {cart.map((product, index) => (
+                      <li key={index}>
+                          <div className="cart-item-info">
+                              <img src={product.image} alt={product.name} />
+                              <h3>{product.name}</h3>
+                          </div>
+                          <p>{formatPrice(product.price)}</p>
+                      </li>
+                  ))}
+              </ul>
+          )}
+          {cart.length > 0 && (
+              <div className="cart-total">
+                  <span>Total:</span>
+                  <span>
+                      {formatPrice(cart.reduce((total, product) => {
+                          if (typeof product.price === "number") {
+                              return total + product.price;
+                          } else {
+                              console.warn("Unexpected price type:", product.price);
+                              return total;
+                          }
+                      }, 0))}
+                  </span>
+              </div>
+          )}
+      </div>
+  );
 };
 
 export default Cart;
+
