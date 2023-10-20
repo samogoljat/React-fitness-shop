3609dc375cf4f4af829985e6c369080f4f79b1bb
samogoljat <samo.goljat1@gmail.com>
2023-10-20 22:29:45 +0200

    added blur to home website

--- fitness-shop-frontend/src/components/Home.js
+++ fitness-shop-frontend/src/components/Home.js
@@ -1,21 +1,23 @@
-// MainContent.js
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link } from 'react-router-dom';
 import '../styles/Home.css';
 
 function MainContent() {
   return (
     <div className="Home-container">
-      <h1>Welcome to Bro's Gym </h1>
-      <p>
-        Join us and begin your journey towards a healthier lifestyle. Oh yeah.
-      </p>
-      {/* Replace button with Link */}
-      <Link to="/about" className="explore-btn">
-        Explore
-      </Link>
+      <div className="Home-content">
+        <h1>Welcome to Bro's Gym</h1>
+        <p>
+          Join us and begin your journey towards a healthier lifestyle. Oh yeah.
+        </p>
+        <Link to="/about" className="explore-btn">
+          Explore
+        </Link>
+      </div>
+      <div className="Home-image-overlay"></div>
     </div>
   );
 }
 
 export default MainContent;
+
